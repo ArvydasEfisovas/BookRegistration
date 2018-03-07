03f5cd84c96d9096eb18233c8d8ad90d0fec1831
bugato <arvydasefisovas@gmail.com>
2018-03-07 02:09:45 +0200

    Added music in background on all activities

---
+++ app/src/main/java/com/example/moksleivis/knygalaboras/BackgroundSoundService.java
@@ -0,0 +1,61 @@
+package com.example.moksleivis.knygalaboras;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+/**
+ * Created by arvyd on 3/6/2018.
+ */
+public class BackgroundSoundService extends Service {
+    private static final String TAG = null;
+    public MediaPlayer player;
+    public IBinder onBind(Intent arg0) {
+
+        return null;
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        player = MediaPlayer.create(this, R.raw.furelise);
+        player.setLooping(true); // Set looping
+        player.setVolume(100,100);
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if(player.isPlaying()){
+           }else{
+            player.start();
+        }
+        return START_NOT_STICKY;
+    }
+
+    public void onStart(Intent intent, int startId) {
+        player.start();
+    }
+    public IBinder onUnBind(Intent arg0) {
+        // TO DO Auto-generated method
+        return null;
+    }
+    public void onPause() {
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(player != null){
+            player.stop();
+            player.release();
+        }else {
+        }
+    }
+
+    @Override
+    public void onLowMemory() {
+
+    }
+
+
+}
